7a3b3f54ba8597c93e066702f5b042a7d547ab58
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-10-28 22:38:38 +0200

    test1

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,12 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
-public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
+public class Ascend{
+	public static void main(String[] args){
+		int x = 5;
+		int y = 8;
+		int z = 25; 
+		System.out.println( x );
+		System.out.println(y );
+		System.out.println(z );
 	}
 }
