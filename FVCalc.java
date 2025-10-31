5e9fa39273b2c8e6c209252fb60b2184104a8814
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-10-31 19:33:32 +0200

    FVC 1

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		double startinginvestment = Double.parseDouble(args[0]);
+		double precentincrease = Double.parseDouble(args[1]);
+		double years = Double.parseDouble(args[2]);
+		double FinalAmount = startinginvestment * Math.pow(1 + precentincrease / 100 , years);
+		System.out.println("After " + years + " years , $" + startinginvestment + " saved at " + precentincrease + "% will yield $" + ((int)FinalAmount));
+
 	}
-}
+
+	}
