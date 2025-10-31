2ceda23a36a3ac629a91482616afbcdc059e00a8
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-10-31 20:49:54 +0200

    Time 3

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,25 +1,19 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
         
         String[] parts = args[0].split(":");
         int hour = Integer.parseInt(parts[0]);
         int minute = Integer.parseInt(parts[1]);
 
-        
+       
         String[] periods = {"AM", "PM"};
         String period = periods[hour / 12];
 
-        
-        
-        int hour12 = (hour % 12 == 0) ? 0 : hour % 12;
-
+       
+        int hour12 = (hour == 0) ? 0 : (hour % 12 == 0 ? 12 : hour % 12);
 
         
         System.out.printf("%d:%02d %s%n", hour12, minute, period);
     }
 }
-
-		
-
